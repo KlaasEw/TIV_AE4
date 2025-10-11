82380ed8f6bd9d4453e4d13d0faa1bd596569969
KlaasEw <klaas.ewald@gmail.com>
2025-10-11 10:22:22 +0200

    Initiale Erstellung

---
+++ 01_Mein_erstes_Programm/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>                 //Include the iostream library for input and output 
+
+using namespace std;                //Standard namespace; avoids prefixing std:: to standard library names
+
+int main()
+{
+    cout << "Hello world!" << endl; 
+    return 0;
+}
+
