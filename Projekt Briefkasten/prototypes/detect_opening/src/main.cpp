37545278c1a5e124701269afda20a82d1e8c7df7
KlaasEw <klaas.ewald@gmail.com>
2026-07-07 18:58:25 +0200

    Bug Fix in  Prototyp Detect_Opening

--- Projekt Briefkasten/prototypes/detect_opening/src/main.cpp
+++ Projekt Briefkasten/prototypes/detect_opening/src/main.cpp
@@ -10,52 +10,52 @@
 
 unsigned long timerAlt = 0;
 unsigned long time_block = 30000;
-bool Reed_State = false;
+bool lastReed = true;
+
+MailSender mail;
+MailConfig cfg = {
+    .wifiSsid       = WIFI_SSID,
+    .wifiPassword   = WIFI_PASSWORD,
+    .wifiBssid      = WIFI_BSSID,
+    .wifiChannel    = 0,
+    .smtpHost       = SMTP_HOST,
+    .smtpPort       = SMTP_PORT,
+    .authorEmail    = AUTHOR_EMAIL,
+    .authorPassword = AUTHOR_PASS,
+    .authorName     = AUTHOR_NAME,
+    .recipientEmail = RECIPIENT_EMAIL,
+    .recipientName  = RECIPIENT_NAME,
+  };
 
 void setup() {
   pinMode(Reed_Pin, INPUT_PULLUP);
   Serial.begin(115200);
   delay(1000);
+
+  lastReed = digitalRead(Reed_Pin);
   
+  if (!mail.begin(cfg)) {
+    Serial.println("Initialisierung fehlgeschlagen.");
+  }
 
   
 }
 
 void loop() {
-  MailSender mail;
-  MailConfig cfg = {
-      .wifiSsid       = WIFI_SSID,
-      .wifiPassword   = WIFI_PASSWORD,
-      .wifiBssid      = WIFI_BSSID,
-      .wifiChannel    = 0,
-      .smtpHost       = SMTP_HOST,
-      .smtpPort       = SMTP_PORT,
-      .authorEmail    = AUTHOR_EMAIL,
-      .authorPassword = AUTHOR_PASS,
-      .authorName     = AUTHOR_NAME,
-      .recipientEmail = RECIPIENT_EMAIL,
-      .recipientName  = RECIPIENT_NAME,
-    };
-
-  if (digitalRead(Reed_Pin) && timerAlt < millis() && Reed_State == false){
-    if (mail.begin(cfg)) {
-      if (mail.send(
-          "Briefkasten geöffnet",
-          "Hey, dein Briefkasten wurde geöffnet!\r\n"
-          "Schau doch mal nach, ob du einen neuen Brief hast.\r\n"
-          "\r\n"
-          "Gesendet von Postbot.")) {
-          Serial.println("E-Mail gesendet.");
-      } else {
-          Serial.println("E-Mail-Versand fehlgeschlagen.");
-      }
+  bool reed = digitalRead(Reed_Pin);
+  if (reed && !lastReed && timerAlt < millis()){
+    Serial.println("Briefkasten Öffnung erkannt");
+    if (mail.send(
+        "Briefkasten geöffnet",
+        "Hey, dein Briefkasten wurde geöffnet!\r\n"
+        "Schau doch mal nach, ob du einen neuen Brief hast.\r\n"
+        "\r\n"
+        "Gesendet von Postbot.")) {
+        Serial.println("E-Mail gesendet.");
     } else {
-      Serial.println("Initialisierung fehlgeschlagen.");
+        Serial.println("E-Mail-Versand fehlgeschlagen.");
     }
     timerAlt = millis() + time_block;
-    Reed_State = true;
   }
-  if (!digitalRead(Reed_Pin)) {
-    Reed_State = false;
-  }
+  lastReed = reed;
 }
